22fc81a60b6c8890032b491ec4f4fd4299d3fdc1
yyy1073 <39354609+yyy1073@users.noreply.github.com>
2024-09-26 23:33:41 -0400

    Create navigation bar

---
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+
+import "./MainNavigation.css";
+
+const MainNavigation = (props) => {
+  return (
+    <MainHeader>
+      <button className="main-navigation__menu-btn">
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">PhotoShare</Link>
+      </h1>
+      <nav className="main-navigation__header-nav">
+        <NavLinks />
+      </nav>
+    </MainHeader>
+  );
+};
+
+export default MainNavigation;
