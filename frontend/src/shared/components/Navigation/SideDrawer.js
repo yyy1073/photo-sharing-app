9a1695d1b23162cfcf1c56a6aef357c9fb963de8
yyy1073 <39354609+yyy1073@users.noreply.github.com>
2024-09-30 13:31:33 -0400

    Sidebar ReactDOM portal

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import reactDOM from "react-dom";
 
 import "./SideDrawer.css";
 
 const SideDrawer = (props) => {
-  return <aside className="side-drawer">{props.children}</aside>;
+  const content = <aside className="side-drawer">{props.children}</aside>;
+
+  return reactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawer;
