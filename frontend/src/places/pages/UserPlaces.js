ce6d6ccec7102e6457aebe4f3183223304805e03
yyy1073 <39354609+yyy1073@users.noreply.github.com>
2024-09-30 14:45:09 -0400

    Add user places view

---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Empire_State_Building_from_the_tower.jpg/1024px-Empire_State_Building_from_the_tower.jpg",
+    address: "20 W 34th St, New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+];
+
+const UserPlaces = () => {
+  return <PlaceList items={DUMMY_PLACES} />;
+};
+
+export default UserPlaces;
